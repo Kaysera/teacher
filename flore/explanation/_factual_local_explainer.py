29b38fcd78910d94c4c98640fe0aa77636df2858
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-02-14 12:43:01 +0100

    Added write_explanation method (without test)

--- flore/explanation/_factual_local_explainer.py
+++ flore/explanation/_factual_local_explainer.py
@@ -9,7 +9,11 @@ class FactualLocalExplainer(BaseExplainer):
         super().__init__()
 
     def write_explanation(self):
-        """TODO"""
+        nlp_exp = f'The element is {self.exp_value[0]} because {self.explanation[0]}\n'
+        for cf in self.explanation[1]:
+            nlp_exp += f'For the element to be {cf.consequent}, you would need {cf.antecedent}\n'
+
+        return nlp_exp
 
     def hit(self):
         if self.exp_value is None or self.target is None:
