d825146703f40c6e1ae324444ee396c0e070eacd
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-02-22 13:19:20 +0100

    Add fuzzy set intersection

--- flore/fuzzy/fuzzy_set.py
+++ flore/fuzzy/fuzzy_set.py
@@ -32,7 +32,41 @@ class FuzzyContinuousSet(FuzzySet):
         return fuzz.trimf(variable, self.fuzzy_points)
 
     def intersection(self, other):
-        pass
+        if not isinstance(other, FuzzyContinuousSet):
+            raise ValueError('Intersection must be between two Fuzzy Sets of the same type')
+
+        A = ((self.fuzzy_points[0], 0), (self.fuzzy_points[1], 1))
+        B = ((self.fuzzy_points[1], 1), (self.fuzzy_points[2], 0))
+
+        C = ((other.fuzzy_points[0], 0), (other.fuzzy_points[1], 1))
+        D = ((other.fuzzy_points[1], 1), (other.fuzzy_points[2], 0))
+
+        all_intersects = [
+                            self._line_intersect(A, C),
+                            self._line_intersect(A, D),
+                            self._line_intersect(B, C),
+                            self._line_intersect(B, D)
+                         ]
+        return max(all_intersects)
+
+    def _line_intersect(self, A, B):
+        if A == B:
+            return 1
+
+        ((Ax1, Ay1), (Ax2, Ay2)) = A
+        ((Bx1, By1), (Bx2, By2)) = B
+
+        denom = (Ax1 - Ax2) * (By1 - By2) - (Ay1 - Ay2) * (Bx1 - Bx2)
+        if denom == 0:
+            return 0
+
+        y = (Ax1 * Ay2 - Ay1 * Ax2) * (By1 - By2) - (Ay1 - Ay2) * (Bx1 * By2 - By1 * Bx2)
+        y /= denom
+
+        if y < 0 or y > 1:
+            return 0
+        else:
+            return y
 
 
 @dataclass
@@ -46,4 +80,7 @@ class FuzzyDiscreteSet(FuzzySet):
             return variable == self.value
 
     def intersection(self, other):
-        pass
+        if not isinstance(other, FuzzyDiscreteSet):
+            raise ValueError('Intersection must be between two Fuzzy Sets of the same type')
+
+        return int(self == other)
