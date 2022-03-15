6c36fd7dc32b23afb9ca16dc31e59f4e515cbccc
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-03-15 17:37:23 +0100

    Added docs for the FactualLocalExplainer

--- teacher/explanation/_factual_local_explainer.py
+++ teacher/explanation/_factual_local_explainer.py
@@ -1,7 +1,17 @@
+"""Factual Local Explainer"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Local application
 from ._base_explainer import BaseExplainer
 from teacher.neighbors import NotFittedError
 
 
+# =============================================================================
+# Classes
+# =============================================================================
 class FactualLocalExplainer(BaseExplainer):
     def __init__(self):
         self.target = None
@@ -9,6 +19,13 @@ class FactualLocalExplainer(BaseExplainer):
         super().__init__()
 
     def write_explanation(self):
+        """Writes the explanation in a human readable manner
+
+        Returns
+        -------
+        str
+            The NLP representation of the explanation
+        """
         nlp_exp = f'The element is {self.exp_value[0]} because {self.explanation[0]}\n'
         for cf in self.explanation[1]:
             nlp_exp += f'For the element to be {cf.consequent}, you would need {cf.antecedent}\n'
@@ -16,6 +33,14 @@ class FactualLocalExplainer(BaseExplainer):
         return nlp_exp
 
     def hit(self):
+        """Returns `True` if the explanation corresponds with the 
+        expected prediction
+
+        Returns
+        -------
+        bool
+            The explanation corresponds with the expected prediction
+        """
         if self.exp_value is None or self.target is None:
             raise(NotFittedError)
         return self.exp_value == self.target
