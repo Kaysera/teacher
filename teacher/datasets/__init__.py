0a0b9363ad4ea6c17e19202486574e28ef3f25b6
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-03-15 17:10:37 +0100

    Added docs to init datasets

--- teacher/datasets/__init__.py
+++ teacher/datasets/__init__.py
@@ -1,5 +1,22 @@
+"""
+The :mod:`teacher.datasets` module includes the different databases
+used to run experiments with the `Teacher` package
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Local application
 from ._base import load_german, load_adult, load_compas, load_heloc, load_beer, load_pima, load_breast
 
+
+# =============================================================================
+# Public objects
+# =============================================================================
+
+# Set the classes that are accessible
+# from the module teacher.datasets
 __all__ = [
     "load_german",
     "load_adult",
