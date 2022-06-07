f97ed1113562303af918752706796cf3fd59fd69
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-06-07 16:54:27 +0200

    Changed theme for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,12 +25,22 @@ author = 'Guillermo Fernandez'
 release = '0.1.0'
 
 
+import sys
+import os
+sys.path.append(os.path.abspath('.'))
+sys.path.append('.')
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx_design",
+    'sphinxext.redirect_from',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,13 +51,17 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The toplevel toctree document (renamed to root_doc in Sphinx 4.0)
+# root_doc = master_doc = 'users/index'
+
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
