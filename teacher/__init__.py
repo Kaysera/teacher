c508a1d374f040ac80b88c6acf6e4ae255efba19
Guillermo Fernandez <Guillermo.Fernandez@uclm.es>
2022-06-08 12:50:12 +0200

    Changes to publish in Pypi

--- teacher/__init__.py
+++ teacher/__init__.py
@@ -18,8 +18,8 @@
 # Dev branch marker is: "X.Y.dev" or "X.Y.devN", where N is an integer.
 # "X.Y.dev0" is the canonical version of "X.Y.dev".
 
-# Scikit-lr package version
-__version__ = "0.1.dev0"
+# Teacher package version
+__version__ = "0.1b1"
 
 
 # =============================================================================
